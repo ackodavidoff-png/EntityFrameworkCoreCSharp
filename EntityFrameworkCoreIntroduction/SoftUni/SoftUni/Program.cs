9b21f7c88830442b4fef65f513271c522ed3344f
Александър Давидов <ackodavidoff@gmail.com>
2026-06-19 11:48:19 +0300

    Initial commit

---
+++ EntityFrameworkCoreIntroduction/SoftUni/SoftUni/Program.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            //Console.WriteLine(GetEmployeesFullInformation(context));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            //Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+            //Console.WriteLine(AddNewAddressToEmployee(context));
+            Console.WriteLine(GetEmployeesInPeriod(context));
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder employees = new StringBuilder();
+            var employeesFullInformation = context.Employees.OrderBy(x => x.EmployeeId).Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.MiddleName,
+                x.JobTitle,
+                x.Salary
+            }).ToList();
+            foreach(var employee in employeesFullInformation)
+            {
+                //Guy Gilbert R Production Technician 12500.00
+                employees.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}"); 
+            }
+            return employees.ToString();
+        }
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var employees = context.Employees.Where(x => x.Salary > 50000).OrderBy(x => x.FirstName).Select(x => new
+            {
+                x.FirstName,
+                x.Salary
+            }).ToList();
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+            return stringBuilder.ToString();
+        }
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var employees = context.Employees.Where(x => x.Department.Name == "Research and Development").OrderBy(x => x.Salary).ThenByDescending(x => x.FirstName).Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.Department,
+                x.Salary
+            });
+            //Gigi Matthew from Research and Development - $40900.00
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Department.Name} - ${employee.Salary:F2}");
+            }
+            return stringBuilder.ToString();
+        }
+        //Do not invoke this method!!!
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Address newAddress = new Address() { TownId = 4, AddressText = "Vitoshka 15" };
+            Employee toChangeName = context.Employees.FirstOrDefault(x => x.LastName == "Nakov");
+            toChangeName.Address = newAddress;
+            //If you want to invoke,make sure the line below is commented
+            context.SaveChanges();
+            var top10Addresses = context.Employees.OrderByDescending(x => x.AddressId).Take(10).Select(x => x.Address.AddressText).ToList();
+            foreach(var address in top10Addresses)
+            {
+                stringBuilder.AppendLine(address);
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var employees = context.Employees.Take(10).Select(x => new
+            {
+                EmployeeFirstName = x.FirstName,
+                EmployeeLastName = x.LastName,
+                ManagerFirstName = x.Manager.FirstName,
+                ManagerLastName = x.Manager.LastName,
+                Projects = x.Projects.Where(y => y.StartDate.Year >= 2001 && y.StartDate.Year <= 2003).Select(y => new
+                {
+                    y.Name,
+                    y.StartDate,
+                    y.EndDate
+                }).ToList()
+            }).ToList();
+            //Guy Gilbert - Manager: Jo Brown
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.EmployeeFirstName} {employee.EmployeeLastName} - Manager: {employee.ManagerFirstName} {employee.ManagerLastName}");
+                foreach(var proj in employee.Projects)
+                {
+                    //"M/d/yyyy h:mm:ss tt" "not finished"
+                    string endDateAsString;
+                    if(proj.EndDate.HasValue)
+                    {
+                        endDateAsString = proj.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt");
+                    }
+                    else
+                    {
+                        endDateAsString = "not finished";
+                    }
+                    //--Half-Finger Gloves - 6/1/2002 12:00:00 AM - 6/1/2003 12:00:00 AM
+                    stringBuilder.AppendLine($"--{proj.Name} - {proj.StartDate.ToString("M/d/yyyy h:mm:ss tt")} - {endDateAsString}");
+                }
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Experimental/Experimental/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace Experimental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string serverName = @".\SQLEXPRESS";
+            Person person1 = new Person(1, "Ivan", "Ivanov", 20, "Sofia");
+            Person person2 = new Person(2, "Georgi", "Georgiev", 30, "Plovdiv");
+            Person person3 = new Person(3, "Dimitar", "Dimitrov", 50, "Vratsa");
+            Console.WriteLine(person1.ToString());
+            Console.WriteLine(person2.ToString());
+            Console.WriteLine(person3.ToString());
+            //AppDbContext.AddRecord(person1);
+        }
+    }
+    public class Person
+    {
+        public Person(int id, string firstName, string lastName, int age, string town)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Town = town;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} with ID {this.Id} and age {this.Age} is from {this.Town}";
+        }
+    }
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        //public void AddRecord(Person person)
+        //{
+        //    People.Add(person);
+        //}
+    }
+}
